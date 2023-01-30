f375cbfd5a8e5e1984c12c95581395b629b63ec0
Cooper <cjo113@uit.no>
2023-01-30 15:49:00 +0100

    set up elems and linked to squares

--- board.py
+++ board.py
@@ -9,22 +9,17 @@ class Board:
         # Nums parameter is a 2D list, like what the sudoku_reader returns
         self.n_rows = len(nums[0])
         self.n_cols = len(nums)
-        self._set_up_squares(nums)
+        self.squares = [[None for _ in range(self.n_rows)] for _ in range(self.n_cols)]
         print("Initialized board")
-
-    def _set_up_squares(self, nums):
-        self.squares = [[Square(num, False if num == 0 else True)
-                        for num in row]
-                        for row in nums]
 
     # Makes it possible to print a board in a sensible format
     def __str__(self):
-        board_str = "Board with " + str(self.n_rows) + " rows and " + str(self.n_cols) + " columns:\n"
+        board_str = f"Board with {self.n_rows} rows and {self.n_cols} columns:\n"
         board_str += "[["
         for row in self.squares:
             for square in row:
                 board_str += square.__str__() + ", "
-            board_str = board_str[:-2] + "]" + "\n ["
+            board_str = board_str[:-2] + "]\n ["
         board_str = board_str[:-3] + "]"
         return board_str
 
@@ -42,12 +37,37 @@ class Square:
 class SudokuBoard(Board):
     def __init__(self, nums):
         Board.__init__(self, nums)
+        self._set_up_squares(nums)
         self._set_up_elems()
 
+    def _set_up_squares(self, nums):
+        self.squares = [
+            [
+                Square(num, False if num == 0 else True)
+                for num in row
+            ]
+            for row in nums
+        ]
+
     def _set_up_elems(self):
-        # You should set up links between your squares and elements
-        # (rows, columns, boxes)
-        print("Setting up elems")
+        row_elems = [Element(row) for row in self.squares]
+        col_elems = [Element([row[j] for row in self.squares]) for j in range(self.n_cols)]
+        box_elems = [
+            [
+                Element([
+                    self.squares[i][j]
+                    for i in range(n, n+3)
+                    for j in range(m, m+3)
+                ])
+                for m in range(0, 9, 3)
+            ]
+            for n in range(0, 9, 3)
+        ] # Boxes are ordered in a 3x3 list as they appear on the board
+
+        for i in range(self.n_rows):
+            for j in range(self.n_cols):
+                assoc_elems = [row_elems[i], col_elems[j], box_elems[int(i/3)][int(j/3)]]
+                self.squares[i][j].elems = assoc_elems
 
     def solve(self):
         # Your solving algorithm goes here!
@@ -60,6 +80,14 @@ class Element:
     def contents(self):
         elem_contents = set([[square.value for square in row] for row in self.squares])
         return elem_contents
+
+    def __str__(self):
+        elem_str = f"Element with {len(self.squares)} squares:\n"
+        elem_str += "["
+        for square in self.squares:
+            elem_str += square.__str__() + ", "
+        elem_str = elem_str[:-2] + "]"
+        return elem_str
 
 if __name__ == "__main__":
     # Test code...
